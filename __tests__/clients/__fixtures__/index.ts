37ccf80d466ebca955ef05f8eb0e3898d59ed58c
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-10-10 19:00:38 +0200

    feat: add wallet resource for disclose

--- __tests__/clients/__fixtures__/index.ts
+++ __tests__/clients/__fixtures__/index.ts
@@ -29,3 +29,29 @@ export const safetypoResponse = { data: { core: "b0b" } };
 export const fingerprintResponse = {
     data: { fingerprint: "a242daa994cc5490020871731d34f7cd3c3993e0b30bac1233d7483001e96e77" },
 };
+
+export const walletId = "D59pZ7fH6vtk23mADnbpqyhfMiJzpdixws";
+
+export const wallet = {
+    address: walletId,
+    publicKey: "03fdc4f413c90c11ca5a780e4695f88706eb991525c7859a63074b1b0009d31da1",
+    username: null,
+    secondPublicKey: null,
+    balance: 99998487,
+    isDelegate: false,
+    vote: null,
+};
+
+export const chainmeta = {
+    height: 151486,
+    timestamp: {
+        epoch: 1920052,
+        unix: 1570634958,
+        human: "2019-10-09T15:29:18.000Z",
+    },
+};
+
+export const walletResponse = {
+    data: wallet,
+    chainmeta,
+};
--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,3 +1,11 @@
 export * from "./response";
 export * from "./client";
-export { NodeStatus, Transaction, Unik, PropertyValue, FingerprintResult, SafeTypoResult } from "./repositories";
+export {
+    NodeStatus,
+    Transaction,
+    Unik,
+    PropertyValue,
+    FingerprintResult,
+    SafeTypoResult,
+    Wallet,
+} from "./repositories";
