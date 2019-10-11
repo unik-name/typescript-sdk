b7a47f7ec5a465cc5fd444f357651fa7ca583561
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-10-11 16:25:18 +0200

    feat: add header uns-network on uniknameclient

--- src/clients/client.ts
+++ src/clients/client.ts
@@ -2,10 +2,14 @@ import { Repository } from "./repository";
 import { Network, Config } from "../config";
 
 export abstract class APIClient {
+    // TODO: Find a way to hide these properties from caller
     public repositories: Record<string, Repository> = {};
     public networkConfig: Config;
+    public headers = {};
 
-    constructor(network: Network = Network.devnet) {
+    protected static DEFAULT_NETWORK: Network = Network.devnet;
+
+    constructor(network: Network = APIClient.DEFAULT_NETWORK) {
         this.networkConfig = this.config()[network];
         this.initRepositories(this);
     }
