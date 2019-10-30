6f2547852507d81e3974537b771b0f147190428c
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-10-30 09:17:48 +0100

    feat: update did parser @ optional type separator '/' replaced by ':'

--- __tests__/functions/did/__fixtures__/parse-fail.ts
+++ __tests__/functions/did/__fixtures__/parse-fail.ts
@@ -1,36 +1,27 @@
 import { DidParserError } from "../../../../src";
 
+const EMPTY_DID_ERROR_MSG = "Empty DID";
 const PATTERN_MATCHING_ERROR_MSG = "DID does not match expected format";
 const INVALID_EXPLICIT_VALUE_FORMAT_MSG = "Invalid explicit value format";
 
 export const shouldFail: any[] = [
     {
-        did: "unik:organization/Space_Elephant?phone",
+        did: "@unikorganization:Space_Elephant?phone",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "unik:individual/bob?*",
+        did: "@unikorganization:Space_Elephant?postalAddress",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unikorganization/Space_Elephant?phone",
+        did: "@unikorganization:Space_Elephant?X509fingerPrint",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unikorganization/Space_Elephant?postalAddress",
-        error: DidParserError,
-        message: PATTERN_MATCHING_ERROR_MSG,
-    },
-    {
-        did: "@unikorganization/Space_Elephant?X509fingerPrint",
-        error: DidParserError,
-        message: PATTERN_MATCHING_ERROR_MSG,
-    },
-    {
-        did: "@unikindividual/bob?*",
+        did: "@unikindividual:bob?*",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
@@ -53,6 +44,11 @@ export const shouldFail: any[] = [
         did: "@",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "",
+        error: DidParserError,
+        message: EMPTY_DID_ERROR_MSG,
     },
     {
         did: ":?",
@@ -80,95 +76,428 @@ export const shouldFail: any[] = [
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@:/?",
+        did: "@::?",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@:/",
+        did: "@::",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@:/?phone",
+        did: "@::?phone",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@/Space_Elephant?*",
+        did: "@:Space_Elephant?*",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unik:organization/bob?phone_number",
+        did: "@unik:organization:bob?phone_number",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unik:organization/bob?phone@number",
+        did: "@unik:organization:bob?phone@number",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
         // Only one property
-        did: "@unik:organization/bob?phone?number",
+        did: "@unik:organization:bob?phone?number",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unik:organization/bob?phone-number",
+        did: "@unik:organization:bob?phone-number",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
         // No space in property
-        did: "@unik:organization/bob?phone number",
+        did: "@unik:organization:bob?phone number",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unik:organization/bob?phone#number",
+        did: "@unik:organization:bob?phone#number",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unik:organization/bob?phoneαnumber",
+        did: "@unik:organization:bob?phoneαnumber",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unik:uns/bob?*",
+        did: "@unik:uns:bob?*",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unik:0/bob?*",
+        did: "@unik:0:bob?*",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unik:4/bob?*",
+        did: "@unik:4:bob?*",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
         did:
-            "@unik:individual/tooLongExplicitValueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee?*",
+            "@unik:individual:tooLongExplicitValueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee?*",
         error: DidParserError,
         message: INVALID_EXPLICIT_VALUE_FORMAT_MSG,
     },
     {
-        did: "@unik:individual/nop:qon?*",
+        did: "@unik:individual:nop:qon?*",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@unik:individual/Space_Elephant?a*b",
+        did: "@unik:individual:Space_Elephant?a*b",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
     {
-        did: "@myUnknownTokenName:individual/Space_Elephant?*",
+        did: "@myUnknownTokenName:individual:Space_Elephant?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+
+    {
+        did: "@unik:organization:/Space_Elephant",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:organization:/Space_Elephant?phone",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:organization:/Space_Elephant?postalAddress",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:organization:/Space_Elephant?X509fingerPrint",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual:/bob?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual:/bob-123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual:/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:1:/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@1:/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@2:/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization:/Space_Elephant",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization:/Space_Elephant?phone",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization:/Space_Elephant?postalAddress",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization:/Space_Elephant?X509fingerPrint",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual:/bob?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:individual:/bob-123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:individual:/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:1:/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "1:/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "2:/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+
+    {
+        did: "@unik:organization/Space_Elephant",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:organization/Space_Elephant?phone",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:organization/Space_Elephant?postalAddress",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:organization/Space_Elephant?X509fingerPrint",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual/bob?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual/bob-123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:1/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@1/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@2/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization/Space_Elephant",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization/Space_Elephant?phone",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization/Space_Elephant?postalAddress",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization/Space_Elephant?X509fingerPrint",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual/bob?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:individual/bob-123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:individual/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:1/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "1/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "2/bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+
+    {
+        did: "@unik:organization/:Space_Elephant",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:organization/:Space_Elephant?phone",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:organization/:Space_Elephant?postalAddress",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:organization/:Space_Elephant?X509fingerPrint",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual/:bob?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual/:bob-123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual/:bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:1/:bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik/:bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@1/:bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@2/:bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization/:Space_Elephant",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization/:Space_Elephant?phone",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization/:Space_Elephant?postalAddress",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:organization/:Space_Elephant?X509fingerPrint",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "@unik:individual/:bob?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:individual/:bob-123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:individual/:bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik:1/:bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "unik/:bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "1/:bob_123?*",
+        error: DidParserError,
+        message: PATTERN_MATCHING_ERROR_MSG,
+    },
+    {
+        did: "2/:bob_123?*",
         error: DidParserError,
         message: PATTERN_MATCHING_ERROR_MSG,
     },
