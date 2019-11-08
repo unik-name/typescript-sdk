07a7e8374ac102ca1cada6785e68641f1128e388
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-11-08 16:53:19 +0100

    feat: allow UNSClient to use custom chain node url

--- src/clients/repositories/types/ServiceRepository.ts
+++ src/clients/repositories/types/ServiceRepository.ts
@@ -1,10 +1,11 @@
 import { Repository } from "../../repository";
+import { UNSConfig, Network } from "../../../config";
 
 export abstract class ServiceRepository extends Repository {
     protected abstract sub(): string;
 
-    protected endpointType(): string {
-        return "service";
+    protected getEndpoint(network: Network): string {
+        return UNSConfig[network].service.url;
     }
 
     protected headers = {
