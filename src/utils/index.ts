b7a8ad646f9f53354d53d33102b19f3b073e6c8e
Fabien TREGUER <fabien.treguer@spacelephant.org>
2019-12-06 11:42:11 +0100

    feat: refactor and add tests

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,17 @@ export function join(...toJoin: string[]) {
 export function merge<T>(a: Partial<T>, b: Partial<T>): T {
     return deepmerge(a || {}, b || {}) as T;
 }
+
+export function computeRequestUrl(baseUrl: string, path: string, query?: string): string {
+    let requestUrl: string = baseUrl;
+
+    if (path) {
+        requestUrl = join(requestUrl, path);
+    }
+
+    if (query) {
+        requestUrl = `${requestUrl}?${query}`;
+    }
+
+    return requestUrl;
+}
