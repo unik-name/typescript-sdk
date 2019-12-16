28b3833c4b216386198de20b68d83a3d51668272
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2019-12-16 14:10:12 +0100

    ci: add nightly publish

---
+++ scripts/publish-nightly.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+# Check new commits since 24h
+if [ $(git log --since="1 day" | wc -l) -eq 0 ]; then
+    echo "Nothing to publish since 1 day.";
+    exit 0;
+fi
+
+# Check current version format
+version=$(grep '"version":' package.json | cut -d\" -f4)
+name=$(grep '"name":' package.json | cut -d\" -f4)
+if [[ $version =~ "-" ]]; then
+    echo "Version format is not X.Y.Z ($version)"
+    exit 1
+fi
+
+echo "Bump package version."
+DATE=$(date +%Y%m%d)
+dev="-dev.$DATE"
+sed  -i '' '/version/s/[^0-9]*$/'"$dev\",/" package.json
+
+echo "Authenticate with registry."
+if [[ -z "$NPM_TOKEN" ]];then
+    echo "Error: NPM_TOKEN is not set."
+    exit 1
+fi
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/repo/.npmrc
+
+echo "Publish package"
+npm publish --tag=dev
+
+echo "Add dist-tags @next"
+npm dist-tag add $name@$version$dev next
