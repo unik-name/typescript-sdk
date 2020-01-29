15778474d75e83c110ac0f9ee9ad899deb1c3be7
Fabien TREGUER <fabien.treguer@spacelephant.org>
2020-01-29 16:44:21 +0100

    feat: limit mint to only one type of unik

--- src/functions/certified-mint.ts
+++ src/functions/certified-mint.ts
@@ -1,15 +1,18 @@
-import { Interfaces, Transactions } from "@uns/ark-crypto";
+import { Interfaces, Transactions, Identities } from "@uns/ark-crypto";
 import {
     CertifiedNftMintTransaction,
     INftMintDemand,
     INftMintDemandCertification,
     UNSCertifiedNftMintBuilder,
+    INftMintDemandPayload,
+    NftMintDemandSigner,
+    ICertifiedDemand,
 } from "@uns/crypto";
 import { Response, UNSClient } from "../clients";
 import { Network } from "../config";
 import { codes } from "../types/errors";
 import { SdkResult } from "../types/results";
-import { Transactions as NftTransactions } from "@uns/core-nft-crypto";
+import { Transactions as NftTransactions, Interfaces as NftInterfaces } from "@uns/core-nft-crypto";
 import { Builders } from "@uns/core-nft-crypto";
 
 export const createCertifiedNnfMintTransaction = async (
@@ -32,12 +35,34 @@ export const createCertifiedNnfMintTransaction = async (
         Transactions.TransactionRegistry.registerTransactionType(CertifiedNftMintTransaction);
 
         builder = new UNSCertifiedNftMintBuilder(nftName, tokenId).properties({ type: tokenType });
+        const currentAsset: NftInterfaces.ITransactionNftAssetData = builder.getCurrentAsset();
 
-        const currentAsset: INftMintDemand = builder.getCurrentAsset() as INftMintDemand;
+        const demandPayload: INftMintDemandPayload = {
+            iss: tokenId,
+            sub: tokenId,
+            iat: Date.now(),
+            cryptoAccountAddress: Identities.Address.fromPassphrase(passphrase),
+        };
 
-        const reponse: Response<INftMintDemandCertification> = await client.mintDemandCertification.create(
-            currentAsset,
-        );
+        const mintDemandSignature: string = new NftMintDemandSigner({
+            ...currentAsset,
+            // here, demand.signature will be ignored by the NftMintDemandSigner
+            // in an ideal world, the NftMintDemandSigner would require type exclusing this property
+            // So, don't worry about the emppty signature here.
+            demand: { payload: demandPayload, signature: "" },
+        }).sign(passphrase);
+
+        const demand: ICertifiedDemand<INftMintDemandPayload> = {
+            payload: demandPayload,
+            signature: mintDemandSignature,
+        };
+
+        const mintDemand: INftMintDemand = {
+            ...currentAsset,
+            demand,
+        };
+
+        const reponse: Response<INftMintDemandCertification> = await client.mintDemandCertification.create(mintDemand);
 
         if (reponse.error) {
             return reponse.error;
@@ -47,7 +72,7 @@ export const createCertifiedNnfMintTransaction = async (
             return codes.MINT_ERROR_CREATION_DATA_NULL;
         }
 
-        builder.certification(reponse.data);
+        builder.demand(demand).certification(reponse.data);
     } else {
         Transactions.TransactionRegistry.registerTransactionType(NftTransactions.NftMintTransaction);
         builder = new Builders.NftMintBuilder(nftName, tokenId).properties({ type: tokenType });
