6136f705d369998305d98c8d3cff12e519ed1e90
Damien Lecan <damien.lecan@spacelephant.org>
2020-02-16 21:32:45 +0100

    doc: add javascript doc to merge function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,6 +11,10 @@ export function escapeSlashes(toEscape: string): string {
     return toEscape.replace(/\//g, "%2F");
 }
 
+/**
+ * According to [`deepmerge#merge` specifications](https://www.npmjs.com/package/deepmerge#mergex-y-options)
+ * priority is given to the second argument (`b` here)
+ */
 export function merge<T>(a: Partial<T> | undefined, b: Partial<T> | undefined): T {
     return deepmerge(a || {}, b || {}) as T;
 }
