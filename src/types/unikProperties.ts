e9cb8f6138bb8da09bc3bae591be77f669997595
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-07-20 11:36:07 +0200

    feat: export Pioneer grades

--- src/types/unikProperties.ts
+++ src/types/unikProperties.ts
@@ -6,3 +6,7 @@ export enum PioneerBadgeGrades {
     INNOVATOR = 1,
     EARLY_ADOPTER = 2,
 }
+
+/* Pioneer badge grades for number of uniks on chain*/
+export const PIONEER_INNOVATOR = 1500;
+export const PIONEER_EARLY_ADOPTER = 150000;
