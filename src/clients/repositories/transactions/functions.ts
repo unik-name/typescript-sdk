6de5153f992840d3e8bfb648e1aa3d86f3337967
Pierre Seznec <pierre.seznec@spacelephant.org>
2020-11-16 18:07:26 +0100

    refactor: ignore transaction schema validation throwing

--- src/clients/repositories/transactions/functions.ts
+++ src/clients/repositories/transactions/functions.ts
@@ -10,9 +10,15 @@ export const transactionsGet = (client: HTTPClient) => (id: string): Promise<Res
 export const transactionsSend = (client: HTTPClient) => (
     transaction: Interfaces.ITransactionData,
 ): Promise<IProcessorResult> => {
-    const { error } = Transactions.Verifier.verifySchema(transaction);
-    if (error) {
-        throw new Error(error);
+    let validation;
+    try {
+        validation = Transactions.Verifier.verifySchema(transaction);
+    } catch (e) {
+        // Unknown transaction type
+        // Could happen in dev environment
+    }
+    if (validation?.error) {
+        throw new Error(validation.error);
     }
     return post<IProcessorResult>(client)(`transactions`, undefined, { transactions: [transaction] });
 };
