c952491d6685b096375fb396ce3fe33d8acc6d40
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2020-11-16 09:58:48 +0100

    fix: user properties filter

--- src/clients/repositories/unik/utils.ts
+++ src/clients/repositories/unik/utils.ts
@@ -6,9 +6,9 @@ const isActiveBadge = (key: string): boolean => ACTIVE_BADGES.includes(key);
 
 const isActiveSystemProperty = (key: string): boolean => ACTIVE_SYSTEM_PROPERTIES.includes(key);
 
-const isUserProperty = (key: string): boolean => key.startsWith(USER_PROPERTY_PREFIX);
+const isUserProperty = (key: string): boolean => key?.startsWith(USER_PROPERTY_PREFIX);
 
-const isVerifiedUrl = (key: string): boolean => key.startsWith(VERIFIED_URL_KEY_PREFIX);
+const isVerifiedUrl = (key: string): boolean => key?.startsWith(VERIFIED_URL_KEY_PREFIX);
 
 export const isActiveProperty = (property: UnikProperty): boolean => {
     const key = Object.getOwnPropertyNames(property)[0];
