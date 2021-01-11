c8445c445214de6999affd09017b2e4531f82c01
Fabien TREGUER <fabien.treguer@spacelephant.org>
2021-01-11 15:35:22 +0100

    chore: add fingerprint computing informations to fingerprint response

--- src/clients/repositories/fingerprint/types.ts
+++ src/clients/repositories/fingerprint/types.ts
@@ -1,3 +1,9 @@
+export type FingerprintComputingInformations = {
+    isEndingWithTLD: boolean;
+    isEndingWithTLDWithDot: boolean;
+};
+
 export type FingerprintResult = {
     fingerprint: string;
+    computingInformations?: FingerprintComputingInformations;
 };
