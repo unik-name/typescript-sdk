181d9487aee4083deb14682eeada217577da27ae
Pierre Seznec <pierre.seznec@spacelephant.org>
2021-05-05 16:23:09 +0200

    refactor: remove duplicated type

--- src/functions/certifications/types.ts
+++ src/functions/certifications/types.ts
@@ -7,13 +7,9 @@ export function isError<T>(result: SdkResult<T>): result is FunctionalError {
     return (result as FunctionalError).code !== undefined && (result as FunctionalError).message !== undefined;
 }
 
-export type UnikPropertiesUpdate = {
-    [_: string]: string | null;
-};
-
 export type UnikUpdateCertifiedTransactionBuildOptions = {
     unikId: string;
-    properties: UnikPropertiesUpdate;
+    properties: Interfaces.INftProperties;
     passphrase: string;
     fees: number;
     nonce: string;
